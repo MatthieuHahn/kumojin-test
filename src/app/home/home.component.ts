7b8a0d44d8df2f612f9388474fbf0a91ccf0d65d
matthew <mhahn@linecoaching.com>
2019-11-28 23:50:13 +0100

    Cleaned up code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,29 +11,49 @@ import { TimeService } from 'src/services/time/time.service';
 export class HomeComponent implements OnInit {
   user: User;
   userId: number;
+  message: String = '';
 
   isUserTimezoneSameAsNavigator: Boolean = false;
   currentUserTime: String = '';
   currentNavigatorTime: String = '';
+  timerId: any;
 
   constructor(private userDataService: UserDataService, private timeService: TimeService) { }
 
   ngOnInit() {
   }
 
+  // ///////////////////////////////////////////////
+
   validateUserId() {
     this.userDataService
       .get(this.userId)
       .subscribe(
-        user => {
-          this.user = user;
-          this.isUserTimezoneSameAsNavigator = this.timeService.isSameTimezoneAsCurrentTimezone(this.user.timezone);
-
-          setInterval(() => {
-            this.currentUserTime = this.timeService.getSpecificTimezoneTime(this.user.timezone);
-            this.currentNavigatorTime = this.timeService.getNowTime();
-          }, 1000);
-        }
+        user => this.displayUserTimes(user),
+        error => this.displayErrorMessage(error)
       );
+  }
+
+  // ///////////////////////////////////////////////
+
+  private displayUserTimes(user) {
+    this.message = 'I found a user !';
+    this.user = user;
+    this.isUserTimezoneSameAsNavigator = this.timeService.isSameTimezoneAsCurrentTimezone(this.user.timezone);
+
+    this.timerId = setInterval(() => {
+      this.currentUserTime = this.timeService.getSpecificTimezoneTime(this.user.timezone);
+      this.currentNavigatorTime = this.timeService.getNowTime();
+    }, 1000);
+  }
+
+  private displayErrorMessage(error) {
+    console.error(error);
+    this.message = 'Oops, no user found';
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+    this.currentUserTime = '';
+    this.currentNavigatorTime = '';
   }
 }
